74f085af3630563a6fff196bb175f63fb81cfea8
Gábor Csárdi <csardi.gabor@gmail.com>
2022-03-19 14:15:41 +0100

    Add arm64 version

--- Casks/rim.rb
+++ Casks/rim.rb
@@ -1,15 +1,25 @@
 cask "rim" do
   version "0.2.0"
-  sha256 "a673fa6b4be75c1f991407a89aad62fbb1035567cb6f40974d771dd4c5f3ffb0"
 
-  url "https://github.com/gaborcsardi/rim/releases/download/v#{version}/rim-#{version}-macOS-x86_64.pkg"
+  if Hardware::CPU.intel?
+    sha256 "a673fa6b4be75c1f991407a89aad62fbb1035567cb6f40974d771dd4c5f3ffb0"
+    url "https://github.com/gaborcsardi/rim/releases/download/v#{version}/rim-#{version}-macOS-x86_64.pkg"
+  else
+    sha256 "8bd39681bd7fa2a0b519afabd1219d04eeff640837227b0f44c4fa067eb0e935"
+    url "https://github.com/gaborcsardi/rim/releases/download/v#{version}/rim-#{version}-macOS-arm64.pkg"
+  end
+
   name "gaborcsardi/rim"
   desc "The R Installation Manager"
   homepage "https://github.com/gaborcsardi/rim"
 
   depends_on macos: ">= :el_capitan"
 
-  pkg "rim-#{version}-macOS-x86_64.pkg"
+  if Hardware::CPU.intel?
+    pkg "rim-#{version}-macOS-x86_64.pkg"
+  else
+    pkg "rim-#{version}-macOS-arm64.pkg"
+  end
 
   uninstall pkgutil: [
               "com.gaborcsardi.rim"
