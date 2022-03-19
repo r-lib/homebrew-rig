554f8ec49f5184b86ea3c8fd6878f109dcd75ce9
Gábor Csárdi <csardi.gabor@gmail.com>
2022-03-19 13:16:14 +0100

    Add intel rim

---
+++ Casks/rim.rb
@@ -0,0 +1,12 @@
+cask "gaborcsardirim" do
+  version "0.2.0"
+  sha256 "a673fa6b4be75c1f991407a89aad62fbb1035567cb6f40974d771dd4c5f3ffb0"
+
+  url "https://github.com/gaborcsardi/rim/releases/download/v#{version}/rim-#{version}-macOS-x86_64.pkg"
+  name "gaborcsardi/rim"
+  desc "The R Installation Manager"
+  homepage "https://github.com/gaborcsardi/rim"
+  license "MIT"
+
+  app ""
+end
