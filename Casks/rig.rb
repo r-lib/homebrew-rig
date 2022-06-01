61bdb7506a291896b6b5d398352219699ce8d6e6
Gábor Csárdi <csardi.gabor@gmail.com>
2022-06-01 07:28:07 +0200

    Fix SHA for arm rig

--- Casks/rig.rb
+++ Casks/rig.rb
@@ -5,7 +5,7 @@ cask "rig" do
     sha256 "0c7f7d8058191adfd79cfd743730e812bf7d49ff7e7da97e974974e12a55161e"
     url "https://github.com/r-lib/rig/releases/download/v#{version}/rig-#{version}-macOS-x86_64.pkg"
   else
-    sha256 "12854794e8cf9aced56873654857a8df13aefad7ed85b20451f3ed32b24a5b93"
+    sha256 "cb494597558d2c1dd2e0f4b6ae69e398d674438ac42909a9c028086a745aac34"
     url "https://github.com/r-lib/rig/releases/download/v#{version}/rig-#{version}-macOS-arm64.pkg"
   end
 
