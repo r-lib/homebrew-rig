d97c52b6b997db3bd102b67eb1fa954c15772871
Gábor Csárdi <csardi.gabor@gmail.com>
2023-04-21 10:17:03 +0200

    rig 0.5.3

--- Casks/rig.rb
+++ Casks/rig.rb
@@ -1,11 +1,11 @@
 cask "rig" do
-  version "0.5.2"
+  version "0.5.3"
 
   if Hardware::CPU.intel?
-    sha256 "a9acac1cf7a07bab5e4a6e93276209d812895e7b9b8aa2b6de2f36afd17ba355"
+    sha256 "3ab2b8a997911761afe66870791d0ca489e3f9b47c9c7e1e8c65b1fd4fb60051"
     url "https://github.com/r-lib/rig/releases/download/v#{version}/rig-#{version}-macOS-x86_64.pkg"
   else
-    sha256 "7e0303deb521c80194d4312dc1e45f5efc57e38fe405cbe0ec1d6be45a56c9ab"
+    sha256 "2ecd10de7a8f06ef5e7dc7dfc37fe8ccd6085f76e3257c59bcff234f85ebe8cc"
     url "https://github.com/r-lib/rig/releases/download/v#{version}/rig-#{version}-macOS-arm64.pkg"
   end
 
