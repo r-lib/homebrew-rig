2c1967d81c7181e4efe22b4bfe4eb8f5e4e5e5c0
Gábor Csárdi <csardi.gabor@gmail.com>
2026-04-24 12:45:29 +0200

    rig 0.8.0

--- Casks/rig.rb
+++ Casks/rig.rb
@@ -1,11 +1,11 @@
 cask "rig" do
-  version "0.7.1"
+  version "0.8.0"
 
   if Hardware::CPU.intel?
-    sha256 "ed6a0c71047a1b45c6ae2cf3ae37c2bfae326ca74efbf6c4420ae541dde1e758"
+    sha256 "ef11eae03c6262bf904842327fc97c2d8321652c01c25d0c2b5167f408c42d0a"
     url "https://github.com/r-lib/rig/releases/download/v#{version}/rig-#{version}-macOS-x86_64.pkg"
   else
-    sha256 "5515f35f4ed1a68df00ec7d641d70da827288fc27672cf49697f6b387e087317"
+    sha256 "9504c7b3f3fdd364c92462115003ec48c36f39425e823b3076b8c1dfe4f11638"
     url "https://github.com/r-lib/rig/releases/download/v#{version}/rig-#{version}-macOS-arm64.pkg"
   end
 
